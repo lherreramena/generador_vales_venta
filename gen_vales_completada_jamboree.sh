42a9be392a168bc73bfb4ec3d92d2572547177fe
Leonardo Herrera <l.herreramena@gmail.com>
2026-07-29 11:29:13 -0400

    vales de jamboree y cafeteria

--- gen_vales_completada_jamboree.sh
+++ gen_vales_completada_jamboree.sh
@@ -11,4 +11,4 @@ name=vales_jamboree
 ./run_python.sh src/gen_letter_2x4.py -c $qty -p "Te/Cafe" "Queque" "Pie/Kuchen" "Sopaipillas" -n ${name}_${qty}_cafeteria.pdf -o assets/img/logo_grupo_scout.png -e assets/img/logo_jamboree.png 
 
 
-./run_python.sh src/gen_letter_2x4.py -c $qty -p Lata_Bebida Completos Promocion -n ${name}_${qty}_completada.pdf -o assets/img/logo_grupo_scout.png -e assets/img/logo_jamboree.png 
+./run_python.sh src/gen_letter_2x4.py -c $qty -p Lata_Bebida Completos Promo_1 Promo_2 -n ${name}_${qty}_completada.pdf -o assets/img/logo_grupo_scout.png -e assets/img/logo_jamboree.png 
